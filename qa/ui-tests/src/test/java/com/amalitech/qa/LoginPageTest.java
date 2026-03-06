512c76096669d14668289b6d6ade14d063b01018
Valens Niyonkuru <valensniyonkuru@example.com>
2026-03-06 17:53:29 +0200

    Fix: Aligned frontend port mapping (3000:3000) and made UI tests more robust with title waits

--- qa/ui-tests/src/test/java/com/amalitech/qa/LoginPageTest.java
+++ qa/ui-tests/src/test/java/com/amalitech/qa/LoginPageTest.java
@@ -7,6 +7,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.testng.annotations.*;
 import static org.testng.Assert.*;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
 
 public class LoginPageTest {
 
@@ -20,12 +23,14 @@ public class LoginPageTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:3000");
     }
 
     @Test
     public void testLoginPageLoads() {
         driver.get("http://localhost:3000/login");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleContains("CommunityBoard"));
+        
         assertTrue(driver.getTitle().contains("CommunityBoard"));
         assertNotNull(driver.findElement(By.cssSelector("input[type=\"email\"]")));
         assertNotNull(driver.findElement(By.cssSelector("input[type=\"password\"]")));
