bf415d4f2f1ec6df6859d034a07b25ae94e92cc1
dusengepeggy <dickinepeggy100@gmail.com>
2026-03-11 18:44:24 +0200

    config fix

--- backend/src/main/java/com/amalitech/communityboard/config/CorsConfig.java
+++ backend/src/main/java/com/amalitech/communityboard/config/CorsConfig.java
@@ -14,6 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
